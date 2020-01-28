01023e4870d5c56fbb23eec49bf55ce2549fa8b7
kulataev_kanat <KulataevKanat01@gmail.com>
2020-01-28 17:09:14 +0600

    ...

--- src/main/java/kg/CSoft/TechnologyTable/endpoint/SearchEndpoint.java
+++
@@ -1,9 +0,0 @@
-package kg.CSoft.TechnologyTable.endpoint;
-
-import kg.CSoft.TechnologyTable.dto.search.SearchDto;
-
-import java.util.List;
-
-public interface SearchEndpoint {
-    List<SearchDto> resultList(String search);
-}
